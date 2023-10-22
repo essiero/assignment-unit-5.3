d086604bcbd7391f5d9bb6221e50722700361b79
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-10-22 18:17:57 -0500

    created function and added first 3 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,45 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
+
+function addToCollection(collection, title, artist, yearPublished) {
+  let albumAdded = {
+    albumTitle: title,
+    albumArtist: artist,
+    albumYearPublished: yearPublished
+  }
+  collection.push(albumAdded);
+  console.log('Album added: ', title, ' by ', artist);
+  console.log('My collection: ', myCollection);
+  return albumAdded;
+
+}
+
+addToCollection(myCollection, 'Juno', 'Remi Wolf', 2021)
+console.log(myCollection);
+addToCollection(myCollection, 'Un Verano Sin Ti', 'Bad Bunny', 2022)
+console.log(myCollection);
+addToCollection(myCollection, 'Acrópole', 'Fillas de Cassandra', 2023);
+console.log(myCollection);
 
 
 
 
 
-
-
+// - Create a function named `addToCollection`. It should have this basic structure:
+//   - ```js
+//       function addToCollection(collection, title, artist, yearPublished) {
+//         // your code here...
+//       }
+//     ```
+//   - This function should:
+//     - Take in a `collection` parameter, so that this function can be used to add to *either collection*
+//     - Take in the album's `title`, `artist`, `yearPublished` as input parameters
+//     - Create a new object having the above properties
+//       - *NOTE*: Your object's properties **must** have `title`, `artist`, and `yearPublished` in order for this assignment's automated tests to work correctly!
+//     - Add the new object to the end of the `collection` array
+//     - `return` the newly created object
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
