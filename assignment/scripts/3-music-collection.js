3f9f4abcd230c6dd1be672aec8472b159f9c383f
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-10-22 18:46:37 -0500

    added 3 more albums and console logged out

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,17 +11,17 @@ function addToCollection(collection, title, artist, yearPublished) {
   }
   collection.push(albumAdded);
   console.log('Album added: ', title, ' by ', artist);
-  console.log('My collection: ', myCollection);
   return albumAdded;
 
 }
 
-addToCollection(myCollection, 'Juno', 'Remi Wolf', 2021)
-console.log(myCollection);
-addToCollection(myCollection, 'Un Verano Sin Ti', 'Bad Bunny', 2022)
-console.log(myCollection);
-addToCollection(myCollection, 'Acrópole', 'Fillas de Cassandra', 2023);
-console.log(myCollection);
+console.log(addToCollection(myCollection, 'Juno', 'Remi Wolf', 2021));
+console.log(addToCollection(myCollection, 'Un Verano Sin Ti', 'Bad Bunny', 2022));
+console.log(addToCollection(myCollection, 'Embruxo', 'Baiuca', 2021));
+console.log(addToCollection(myCollection, 'Pang', 'Caroline Polachek', 2019));
+console.log(addToCollection(myCollection, 'YHLQMDLG', 'Bad Bunny', 2020));
+console.log(addToCollection(myCollection, 'We Love Dogs!', 'Remi Wolf', 2021));
+console.log('My music collection:', myCollection);
 
 
 
@@ -40,6 +40,29 @@ console.log(myCollection);
 //       - *NOTE*: Your object's properties **must** have `title`, `artist`, and `yearPublished` in order for this assignment's automated tests to work correctly!
 //     - Add the new object to the end of the `collection` array
 //     - `return` the newly created object
+
+// - Use and Test the `addToCollection` function:
+//   - Add 6 albums to **your** collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+//   - `console.log`` each album as added using the function's returned value.
+//   - After all are added, console.log the `collection` array.
+
+// - Create a function named `showCollection`. This function should:
+//   - Take in a `collection` parameter. (This allows it to be reused to show any array of album objects!)
+//   - Loop over the `collection` and `console.log` each album's information formatted **within a single string**, like: `TITLE by ARTIST, published in YEAR`.
+
+// - Test the `showCollection` function.
+
+// - Add a function named `findByArtist`. This function should:
+//   - Take in a `collection` parameter. Remember, we want to be able to search any collection!
+//   - Take in an `artist` (string) parameter
+//   - Create an empty array to hold any matching results, if any
+//   - Loop through the `collection` and add any album objects with a matching artist to the array.
+//   - Return the array with the matching results. (If no results are found, an empty array should be returned.)
+
+// - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are returned.
+
+// > When testing your functions, write all tests in the JavaScript file!
+
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
